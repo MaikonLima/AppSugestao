852ada48bbfacf500e9ea5130da5dc282c5294ce
Maikon Guimaraes <andersonmaikon93@gmail.com>
2022-11-23 16:00:42 -0400

    feat: form new user

--- app/src/main/java/com/maik/org/appsugestao/FormUsuario.java
+++ app/src/main/java/com/maik/org/appsugestao/FormUsuario.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginUsuario extends AppCompatActivity {
+public class FormUsuario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_usuario);
+        setContentView(R.layout.activity_form_usuario);
     }
-
-
 }
--- app/src/main/java/com/maik/org/appsugestao/MainActivity.java
+++ app/src/main/java/com/maik/org/appsugestao/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,5 +37,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(nextPage);
             }
         });
+
+
+        TextView tvNewUse = findViewById(R.id.tvNewUser);
+        tvNewUse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newForm = new Intent(getApplicationContext(), FormUsuario.class);
+                startActivity(newForm);
+            }
+        });
+
     }
 }
